94637fae35fef19d0458af741b62c2ed81bd82d7
minkimcello <minkimcello@gmail.com>
2020-11-17 10:22:38 -0500

    Jonas

--- cypress/integration/interactor.spec.js
+++ cypress/integration/interactor.spec.js
@@ -2,11 +2,22 @@
 import { Button, Heading, TextField } from 'bigtest';
 
 describe('Cypress with Interactors', () => {
-  it('displays submitted data from modal', () => {
-    cy.visit('/');
-    cy.interactor(Button('SIGN IN'), 'click');
-    cy.interactor(TextField({ placeholder: 'USERNAME' }), 'fillIn', 'batman');
-    cy.interactor(Button('SUBMIT'), 'click');
-    cy.interactor(Heading('Hello batman.'), 'exists');
+  beforeEach(() => {
+    cy.visit('/')
+    cy.do(Button('SIGN IN').click())
+  });
+  describe('displays submitted data from modal', () => {
+    it('check name batman', () => {
+      cy.do([
+        TextField({ placeholder: 'USERNAME' }).fillIn('batman'),
+        TextField({ placeholder: 'USERNAME' }).fillIn('batman2'),
+      ])
+      cy.do(Button('SUBMIT').click())
+      cy.expect(Heading('Hello batman2.').exists());
+      cy.expect([
+        Heading('Hello batman2.').exists(),
+        Heading('Hello batman2.').exists()
+      ]);
+    })
   });
 });
